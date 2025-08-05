29386c36d5c3cbfb659820e57ec0709831ea1fea
joseDelgado <jose.delgado@correo.unimet.edu.ve>
2025-08-05 13:57:23 -0400

    Update Museo.py

---
+++ Museo.py
@@ -0,0 +1,17 @@
+class Museo:
+    def __init__(self):
+        self.deptos = []
+        self.autores = []
+        self.obras = []
+        self.nacionalidades = []
+
+    def cargar_datos(self):
+        pass
+
+    def iniciar(self):
+        #Cargar los departamentos y el CSV
+        self.cargar_datos()
+
+        #Mostrar el menu dentro del While True
+        while True:
+            break
