1d3675402792f7d3386909b31c6ce77b819d66c0
joseDelgado <jose.delgado@correo.unimet.edu.ve>
2025-08-05 22:24:13 -0400

    Update Museo.py

--- Museo.py
+++ Museo.py
@@ -1,3 +1,7 @@
+import requests
+from Departamento import Departamento
+from Autor import Autor
+
 class Museo:
     def __init__(self):
         self.deptos = []
@@ -6,6 +10,71 @@ class Museo:
         self.nacionalidades = []
 
     def cargar_datos(self):
+        response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
+
+        data = response.json()
+
+        # Recorremos cada departamento recibido
+        for info_depto in data["departments"]:
+
+            depto_id = info_depto["departmentId"]       
+            nombre = info_depto["displayName"]
+
+            # Creamos la instancia y la guardamos en la lista de deptos
+            self.deptos.append(Departamento(depto_id, nombre))
+
+        #Luego cargar el CSV con las nacionalidades
+
+    def obras_depto(self):
+
+        #Mostrar los departamentos disponibles
+        print("\nDepartamentos Disponibles:")
+        for depto in self.deptos:
+            print(f"{depto.id} - {depto.nombre}")
+
+        #Elegir el departamento a traves del id
+        opcion = input("Ingresa el id del depto: ")
+        while not opcion.isnumeric() or self.buscar_depto_id(int(opcion)) == None:
+            print("Error! El id debe ser numerio y debe ser un id valido.")
+            opcion = input("Ingresa el id del depto: ")
+
+        #Buscar y obtener una instacia de la clase Departamento a traves del id
+        depto_selecccionado = self.buscar_depto_id(int(opcion))
+        
+        #Buscar las obras correspondientes al id seleccionado por el usuario
+        response = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds={depto_selecccionado.id}")
+
+        #Respuesta de la API en json
+        data = response.json()
+
+        #Total de obras obtenidas
+        total_obras = len(data["objectIDs"])
+        num_lote = 0 #numero de lotes el cual ira varian de 10 en 1o
+
+        #Iterar mientra el ote sea menor al total de las obras
+        while num_lote < total_obras:
+            
+            #Obtener un sublista el cual ira desde el num_lote de la lista de obras hasta el numero de lote mas 10
+            ids_actual = data["objectIDs"][num_lote: num_lote+10]
+            
+            #iterar sobre la lista de ids correspondiente al bloque actual para mostrarlas al usuario
+            for id_act in ids_actual:
+                response_obra = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{id_act}")
+
+                data_obra = response_obra.json()
+
+    def buscar_depto_id(self, id):
+        if len(self.deptos) != 0:
+            for depto in self.deptos:
+                if depto.id == id:
+                    return depto
+        
+        return None
+
+    def obras_nacionalidad(self):
+        pass
+
+    def obras_autor(self):
         pass
 
     def iniciar(self):
@@ -14,4 +83,22 @@ class Museo:
 
         #Mostrar el menu dentro del While True
         while True:
-            break
+            print("\n**************************************")
+            print("       Bienvenidos a MetroArt")
+            print("**************************************")
+
+            print("1. Ver lista de obras por Departamento.\n2. Ver lista de obras por Nacionalidad del autor.\n3. Ver lista de obras por nombre del autor.\n4. Salir")
+
+            opcion = input("\nIngrese la opcion deseada: ")
+            
+            if opcion == "1":
+                self.obras_depto()
+            elif opcion =="2":
+                self.obras_nacionalidad()
+            elif opcion=="3":
+                self.obras_autor()
+            elif opcion=="4":
+                print("\nGracias por utilizar MatroArt.")
+                break
+            else:
+                print("\nOpcion Invalida!\n")
